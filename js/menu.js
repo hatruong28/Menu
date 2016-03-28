004fdcee732c2acf44b28b936ee820f8e468da90
Truong Ha <truong.ha.k1set@eiu.edu.vn>
2016-03-28 15:04:21 +0700

    Update

--- js/menu.js
+++ js/menu.js
@@ -15,39 +15,69 @@
  ;(function($, window, undefined) {
  	'use strict'
 
- 	var pluginName = 'menu';
+ 	var pluginName = 'menu',
+ 		mainMenuCls = 'main-menu',
+ 		subMenuCls = 'sub-menu',
+ 		activeMenuCls = 'active-menu',
+ 		curMenuCls = 'current-menu',
+ 		timeOut,
+ 		currentMenu;
 
- 	function Menu(element, options){
+ 	function Plugin(element, options){
  		this.element = $(element);
  		this.options = $.extend({}, $.fn[pluginName].defaults, this.element.data, options);
  		this.init();
  	}
 
- 	Menu.prototype = {
+ 	Plugin.prototype = {
  		init: function() {
  			var that = this,
- 				options = this.options,
- 				sub;
+ 				options = this.options;
 
- 			var subMenu = this.element.find('li > ul');
- 			this.element.children().addClass('main-menu');
+ 			that.initMenu();
 
+ 			that.element.find('li').on('mouseenter.menu', function(e) {
+ 				var newChild = $(this).data('child');
+ 				clearTimeout(timeOut);
+ 				that.element.find('li.' + options.activeClass).removeClass(options.activeClass);
+ 				if(currentMenu){
+					that.hideMenu(currentMenu);
+				}
+ 				if(newChild) {
+ 					currentMenu = newChild;
+ 					that.showMenu(newChild);
+ 				} else {
+ 					$(this).addClass(options.activeClass);
+ 				}
+ 			}).on('mouseleave.menu', function() {
+ 				timeOut = setTimeout(function() {
+ 					that.element.find('li.' + options.activeClass).removeClass(options.activeClass);
+ 					that.hideMenu($('div.' + activeMenuCls).first());
+ 				}, options.hideDelay);
+ 			});
+
+ 			//that.showMenu($('div').eq(4));
+ 		},
+ 		initMenu: function(){
+ 			var that = this,
+ 				options = that.options,
+ 				subMenu = that.element.find('.' + subMenuCls);
+ 			that.element.children().addClass(mainMenuCls);
  			subMenu.each(function() {
  				var parent = $(this).parent(),
  					parentPos = parent.position(),
  					pos;
- 				if($(this).parent().hasClass('main-menu')){
- 					pos = {left: parentPos.left, top: parentPos.top + 42};
+ 				if($(this).parent().hasClass(mainMenuCls)){
+ 					pos = {left: parentPos.left, top: parentPos.top + parent.outerHeight()};
  					$(this).css({
 	 					'position': 'absolute',
 	 					'top': '0',
 	 					'margin-top': -$(this).outerHeight()
 	 				});
-	 				$(this).addClass('first-sub');
  				} else {
  					parentPos = parent.parent().data('pos');
- 					var top = parent.index() * 41 + parentPos.top,
- 						left = parentPos.left + parent.outerWidth() + 1;
+ 					var top = parent.index() * parent.outerHeight() + parentPos.top,
+ 						left = parentPos.left + parent.outerWidth() + 3;
  					pos = {left: left, top: top};
  					$(this).css({
 	 					'position': 'absolute',
@@ -68,236 +98,102 @@
 					'overflow':'hidden',
 					'left': pos.left,
 					'top': '-1000px',
-					'width': $(this).outerWidth(),
-					'height': $(this).outerHeight() + 4
+					'width': $(this).outerWidth() + 1,
+					'height': $(this).outerHeight() + 1
 				});
 				child.parent().data('child', child);
 				child.data('parent', child.parent());
 				child.appendTo($('body'));
  			});
-
- 			/*$('#nav li').on('mouseenter', function() {
- 				$('.active').removeClass('active');
- 				$(this).addClass('active');
- 				var menu = $('.active-menu');
- 				menu.each(function(index) {
- 					var that = $(this);
- 					if(index == 0){
- 						$(this).removeClass('active-menu');
-						$(this).children().stop().animate({
-							'margin-top': -$(this).outerHeight()
-						}, options.duration, function() {
-							that.css('top', '-1000px');
-						});
- 					} else {
- 						$(this).removeClass('active-menu');
-						$(this).children().stop().animate({
-							'margin-left': -$(this).outerWidth()
-						}, options.duration, function() {
-							that.css('top', '-1000px');
-						});
- 					}
- 				})
- 				var newChild = $(this).data('child');
- 				if(newChild){
- 					newChild.addClass('active-menu');
- 					var childPos = newChild.data('pos');
- 					newChild.css('top', childPos.top);
-					newChild.children().stop().animate({
- 						'margin-top': 0
- 					}, options.duration);
- 					$('.active-menu li').off('mouseenter').on('mouseenter', mouseenter);
- 				}
- 			});
-
- 			var mouseenter = function() {
- 				var newChild = $(this).data('child');
- 				$('.current-menu').removeClass('current-menu');
- 				$(this).closest('div.active-menu').find('.active').removeClass('active');
- 				$(this).addClass('active');
- 				$(this).closest('div.active-menu').addClass('current-menu');
- 				if($('div.active-menu').length > 1 
- 					&& $(this).closest('div.active-menu')[0] !== $('div.active-menu').last()[0]){
- 					var activeMenu = $('.active-menu'),
- 						current = activeMenu.filter('.current-menu'),
- 						indexMenu = activeMenu.index(current);
- 					activeMenu.each(function(index) {
- 						if(index > indexMenu)
- 							mouseleave($(this));
- 					});
- 				}
- 				if(newChild){
- 					newChild.addClass('active-menu');
- 					var pos = newChild.data('pos');
- 					newChild.css('top', pos.top);
-					newChild.children().stop().animate({
-						'margin-left': 0
-					}, options.duration, 'swing');
- 					$('.active-menu li').off('mouseenter').on('mouseenter', mouseenter);
- 				}
- 			};*/
- 			var currentMenu;
-
- 			that.element.find('li').on('mouseenter', function(e) {
- 				var newChild = $(this).data('child');
- 				if(currentMenu){
-					that.hideMenu(currentMenu);
-				}
- 				if(newChild) {
- 					currentMenu = newChild;
- 					that.showMenu(newChild);
- 				} else {
- 					$(this).parent().find('.active').removeClass('active');
- 					$(this).addClass('active');
- 				}
- 			});
-
- 			var showMenu = function(menu) {
- 				menu.addClass('active-menu');
-				var pos = menu.data('pos'),
-					li = menu.data('parent'),
-					margin = {};
-				menu.css('top', pos.top);
-				$('.active-menu li').off('mouseenter').on('mouseenter', function() {
-					var newChild = $(this).data('child');
-					if($(this).closest('div.active-menu')[0] !== $('div.active-menu').last()[0]) {
-						$(this).closest('div.active-menu').addClass('current-menu');
-	 					var activeMenu = $('.active-menu'),
-	 						current = activeMenu.filter('.current-menu'),
-	 						indexMenu = activeMenu.index(current);
-	 					$('.current-menu').removeClass('current-menu');
-	 					that.hideMenu($(activeMenu[indexMenu + 1]));
-	 				}
-	 				$(this).closest('div.active-menu').find('.active').removeClass('active');
-	 				$(this).addClass('active');
- 					if(newChild){
- 						showMenu(newChild);
- 					}
-				});
-				if(li.hasClass('main-menu')){
-					$('#nav').find('li.active').removeClass('active');
-					li.addClass('active');
-					margin = {'margin-top': 0};
-				} else {
-					margin = {'margin-left': 0};
-					if(!li.hasClass('active')){
-						li.closest('div').find('.active').removeClass('active');
-						li.addClass('active');
-						showMenu(li.closest('div'));
-					}
-				}
-				menu.children().stop().animate(margin, options.duration);
-				$('div.active-menu').off('mouseleave').on('mouseleave', function() {
-		 			var menu = $('div.active-menu').first();
-		 			that.element.find('li.active').removeClass('active');
-		 			that.hideMenu(menu);
-		 		});
- 			};
-
- 			var hideMenu = function(menu) {
- 				menu.addClass('current-menu');
- 				var activeMenu = $('.active-menu'),
-					current = activeMenu.filter('.current-menu'),
-					indexMenu = activeMenu.index(current);
-				menu.removeClass('current-menu');
-				activeMenu.each(function(index) {
-					var that = $(this);
-					if(index >= indexMenu){
-						var li = that.data('parent'),
-							margin = {};
-						that.find('.active').removeClass('active');
-						that.removeClass('active-menu');
-						if(li.hasClass('main-menu')){
-							margin = {'margin-top': -$(this).outerHeight()};
-		 				} else {
-		 					margin = {'margin-left': -$(this).outerWidth()};
-		 				}
-		 				that.children().stop().animate(
-		 					margin,
-		 					options.duration,
-		 					function() {
-								that.css('top', '-1000px');
-							}
-						);
-					}
-				});
- 			};
-
- 			var mouseleave = function(menu) {
- 				menu.find('.active').removeClass('active');
- 				menu.removeClass('active-menu');
-				menu.children().stop().animate({
-					'margin-left': -menu.outerWidth()
-				}, options.duration, function() {
-					menu.css('top', '-1000px');
-				});
- 			};
-
  		},
  		showMenu: function(menu) {
  			var that = this,
- 				options = that.options;
- 			menu.addClass('active-menu');
-			var pos = menu.data('pos'),
+ 				options = that.options,
+ 				pos = menu.data('pos'),
 				li = menu.data('parent'),
-				margin = {};
+				margin = {},
+				activeMenu = 'div.' + activeMenuCls,
+				activeLi = '.' + activeMenuCls + ' li';
+			if(!menu.hasClass(activeMenuCls)){
+				menu.css('opacity', 0.7);
+			}
 			menu.css('top', pos.top);
-			$('.active-menu li').off('mouseenter').on('mouseenter', function() {
-				var newChild = $(this).data('child');
-				//that.showMenu($(this).closest('div.active-menu'));
-				if($(this).closest('div.active-menu')[0] !== $('div.active-menu').last()[0]) {
-					$(this).closest('div.active-menu').addClass('current-menu');
- 					var activeMenu = $('.active-menu'),
- 						current = activeMenu.filter('.current-menu'),
- 						indexMenu = activeMenu.index(current);
- 					$('.current-menu').removeClass('current-menu');
- 					that.hideMenu($(activeMenu[indexMenu + 1]));
- 				}
- 				$(this).closest('div.active-menu').find('.active').removeClass('active');
- 				$(this).addClass('active');
-				if(newChild){
-					that.showMenu(newChild);
-				}
-			});
-			if(li.hasClass('main-menu')){
-				$('#nav').find('li.active').removeClass('active');
-				li.addClass('active');
+			menu.addClass(activeMenuCls);
+			if(li.hasClass(mainMenuCls)){
+				if(!currentMenu)
+					currentMenu = menu;
+				that.element.find('li' + options.activeClass).removeClass(options.activeClass);
+				li.addClass(options.activeClass);
 				margin = {'margin-top': 0};
 			} else {
 				margin = {'margin-left': 0};
-				if(!li.hasClass('active')){
-					li.closest('div').find('.active').removeClass('active');
-					li.addClass('active');
+				if(!li.hasClass(options.activeClass)){
+					li.closest('div').find('.' + options.activeClass).removeClass(options.activeClass);
+					li.addClass(options.activeClass);
 					that.showMenu(li.closest('div'));
 				}
 			}
-			menu.children().stop().animate(margin, options.duration);
+			menu.children().stop().animate(margin, options.duration, function(){
+				menu.css('opacity', 1);
+			});
+
+			$(activeLi).off('mouseenter.menu').on('mouseenter.menu', function() {
+				var newChild = $(this).data('child'),
+					mainMenu = that.element.find('li.' + mainMenuCls);
+				clearTimeout(timeOut);
+				mainMenu.each(function() {
+					if(!$(this).hasClass(options.activeClass) && $(this).data('child')){
+						that.hideMenu($(this).data('child'));
+					}
+				});
+				that.showMenu($(this).closest(activeMenu));
+				if($(this).closest(activeMenu)[0] !== $(activeMenu).last()[0]) {
+					$(this).closest(activeMenu).addClass(curMenuCls);
+ 					var current = $(activeMenu).filter('.' + curMenuCls),
+ 						indexMenu = $(activeMenu).index(current);
+ 					$('.' + curMenuCls).removeClass(curMenuCls);
+ 					that.hideMenu($($(activeMenu)[indexMenu + 1]));
+ 				}
+ 				$(this).closest(activeMenu)
+ 					.find('.' + options.activeClass)
+ 					.removeClass(options.activeClass);
+ 				$(this).addClass(options.activeClass);
+				if(newChild){
+					that.showMenu(newChild);
+				}
+			}).off('mouseleave.menu').on('mouseleave.menu', function() {
+				timeOut = setTimeout(function() {
+					that.element.find('li.' + options.activeClass).removeClass(options.activeClass);
+ 					that.hideMenu($(activeMenu).first());
+ 				}, options.hideDelay);
+			});
  		},
  		hideMenu: function(menu) {
  			var that = this,
  				options = that.options;
- 			menu.addClass('current-menu');
-			var activeMenu = $('.active-menu'),
-				current = activeMenu.filter('.current-menu'),
+ 			menu.addClass(curMenuCls);
+			var activeMenu = $('.' + activeMenuCls),
+				current = activeMenu.filter('.' + curMenuCls),
 				indexMenu = activeMenu.index(current);
-			menu.removeClass('current-menu');
+			menu.removeClass(curMenuCls);
 			activeMenu.each(function(index) {
 				var that = $(this);
 				if(index >= indexMenu){
 					var li = that.data('parent'),
 						margin = {};
-					that.find('.active').removeClass('active');
-					if(li.hasClass('main-menu')){
-						margin = {'margin-top': -$(this).outerHeight()};
+					that.find('.' + options.activeClass).removeClass(options.activeClass);
+					that.data('parent').removeClass(options.activeClass);
+					if(li.hasClass(mainMenuCls)){
+						margin = {'margin-top': -that.outerHeight()};
 	 				} else {
-	 					margin = {'margin-left': -$(this).outerWidth()};
+	 					margin = {'margin-left': -that.outerWidth()};
 	 				}
 	 				that.children().stop().animate(
 	 					margin,
 	 					options.duration,
 	 					function() {
 							that.css('top', '-1000px');
-							that.removeClass('active-menu');
+							that.removeClass(activeMenuCls);
 						}
 					);
 				}
@@ -312,7 +208,7 @@
  		return this.each(function() {
  			var intance = $.data(this, pluginName);
  			if (!intance) {
- 				$.data(this, pluginName, new Menu(this, options));
+ 				$.data(this, pluginName, new Plugin(this, options));
  			} else if (intance[options]){
  				intance[options](params);
  			}
@@ -320,8 +216,9 @@
  	}
 
  	$.fn[pluginName].defaults = {
- 		duration: 200,
- 		fadeOutTime: 1000
+ 		duration: 300,
+ 		hideDelay: 2000,
+ 		activeClass: 'active'
  	};
 
  	$(function() {
